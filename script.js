a20165b119933197802d4804bceb2c918187d933
Hareshku <hareesh7737@gmail.com>
2024-12-12 20:21:47 +0500

    few changes made

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed(".text", {
-    strings: ["Programming" , "Cybersecurity" , "Web Development"],
+    strings: ["Programming" , "Data Science" , "Web Development"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
